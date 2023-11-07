51fbabebc2579285e88df09706f170819baa2f00
vishnuvvr7670 <vishnuvvr7670@gmil.com>
2023-11-07 20:14:00 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def MaheshBabu(request):
+    return HttpResponse('<h1><marquee> MaheshBabu is the one of best actor in the telugufilm industry..</h1></marquee>')
